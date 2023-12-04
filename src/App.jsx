e07e0e0fb156958646c5fd52a35525e77d703cf8
hugogaytan10 <estardus9@gmail.com>
2023-12-04 14:40:06 -0600

    arreglo de bug em caracteristicas

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { Banner } from './Components/Banner'
 import { BannerSecundario } from './Components/BannerSecundario'
 import { Contacto } from './Components/Contacto'
 import { Productos } from './Components/Productos'
-import { Caracteristicas } from './Components/caracteristicas'
+import { Caracteristicas } from './Components/Caracteristicas'
 
 function App() {
 
