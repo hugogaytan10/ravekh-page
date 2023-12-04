e464ba723d2d154e85d2e63ceea8e0f1a9b24de4
hugogaytan10 <estardus9@gmail.com>
2023-12-04 17:40:01 -0600

    agregacion de flecha

--- src/Components/Banner.jsx
+++ src/Components/Banner.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
-
+import flecha from '../assets/arrow-forward.svg';
 export const Banner = () => {
     return (
         <div className='bg-azul-banner banner'>
             <h1 className='text-gray-50 font-bold text-4xl text-center p-8'>Crea tu campaña publicitaria o página web</h1>
             <p className='w-3/4 m-auto text-left text-gray-50 font-thin md:text-center'>Lleva tu negocio al siguiente nivel con nosotros</p>
             <a className='bg-gray-50 mt-10 rounded-md p-1 text-azul-banner font-bold flex w-2/4 justify-around m-auto items-center md:w-2/12 btn-empezar'>Empezar
-                <img src='../assets/arrow-forward.svg' alt='flecha' />
+                <img src={flecha} alt='flecha' />
             </a>
         </div>
     )
