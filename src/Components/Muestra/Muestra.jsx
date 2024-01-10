7db162272fa006c9243711c84c3ba0295a37ea01
Clementain <clentecarmona@gmail.com>
2024-01-09 20:46:37 -0600

    Se agrega el carrusel en las demos de productos que hemos hecho

--- src/Components/Muestra/Muestra.jsx
+++ src/Components/Muestra/Muestra.jsx
@@ -1,72 +1,91 @@
-import React from 'react'
-import { CardsProjects } from './CardsProjects'
-import mascotitas from '../../assets/MASCOTITAS.png'
-import filter from '../../assets/filter-cards.png'
-import itsur from '../../assets/ITSUR.png'
-import short from '../../assets/short-link.png'
-import webComida from '../../assets/webComida.png'
+import React from 'react';
+import { CardsProjects } from './CardsProjects';
+import mascotitas from '../../assets/MASCOTITAS.png';
+import filter from '../../assets/filter-cards.png';
+import itsur from '../../assets/ITSUR.png';
+import short from '../../assets/short-link.png';
+import webComida from '../../assets/webComida.png';
+import ecomers from '../../assets/ecomers.png';
+import Slider from 'react-slick';
+
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+
 export const Muestra = () => {
-    return (
-        // <div className='flex flex-wrap w-3/4 m-auto justify-around mt-32   rounded-lg  lg:gap-1'>
-        //     <h3 className='text-center text-2xl font-bold md:text-3xl block w-full'>Algunos Demos</h3>
-        //    
-        //   
-        //     <CardsProjects
-        //         title={'Acortador de URL'}
-        //         description={'Demo de consumo de apis'}
-        //         imageUrl={short}
-        //         link='https://cute-boba-d3d365.netlify.app/'
-        //     />
-        //     <CardsProjects
-        //             title={'Portal de comida'}
-        //             description={'Portal de comida (responsive)'}
-        //             imageUrl={webComida}
-        //             link='https://main--stalwart-narwhal-93eefb.netlify.app/'
-        //         />
-        // </div>
-  <div className="carousel rounded-box w-96">
-  <div className="carousel-item w-1/2">-
-  <CardsProjects className='w-full'
-                   title={'Portal de comida'}
-                     description={'Portal de comida (responsive)'}
-                     imageUrl={webComida}
-                     link='https://main--stalwart-narwhal-93eefb.netlify.app/'
-                 />
-  </div> 
-  <div className="carousel-item w-1/2">
-  <CardsProjects
-                title={'Mascotitas'}
-                 description={'Demo de una tienda para mascotas'}
-                 imageUrl={mascotitas}
-                 link='https://hugogaytan10.github.io/Mascotitas'
-             />
-  </div> 
-  <div className="carousel-item w-1/2">
-    
-        <CardsProjects
-             title={'Filtro de tarjetas'}
-             description={'Demo de un filtro de tarjetas'}
-             imageUrl={filter}
-                link='https://teal-jalebi-6ba797.netlify.app/'
-         />
-  </div> 
-  <div className="carousel-item w-1/2">
-  <CardsProjects
-                 title={'Página escolar'}
-                 description={'Demo de página'}
-                 imageUrl={itsur}
-                 link='https://main--melodic-sundae-a09a6c.netlify.app/'
-             />
-  </div> 
-  <div className="carousel-item w-1/2">
-    <img src="https://daisyui.com/images/stock/photo-1550258987-190a2d41a8ba.jpg" className="w-full" />
-  </div> 
-  <div className="carousel-item w-1/2">
-    <img src="https://daisyui.com/images/stock/photo-1559181567-c3190ca9959b.jpg" className="w-full" />
-  </div> 
-  <div className="carousel-item w-1/2">
-    <img src="https://daisyui.com/images/stock/photo-1601004890684-d8cbf643f5f2.jpg" className="w-full" />
-  </div>
-</div>
-    )
-}
+  const projects = [
+    {
+      title: 'Portal de comida',
+      description: 'Portal de comida (responsive)',
+      imageUrl: webComida,
+      link: 'https://main--stalwart-narwhal-93eefb.netlify.app/',
+    },
+    {
+      title: 'Mascotitas',
+      description: 'Demo de una tienda para mascotas',
+      imageUrl: mascotitas,
+      link: 'https://hugogaytan10.github.io/Mascotitas',
+    },
+    {
+      title: 'Filtro de tarjetas',
+      description: 'Demo de un filtro de tarjetas',
+      imageUrl: filter,
+      link: 'https://teal-jalebi-6ba797.netlify.app/',
+    },
+    {
+      title: 'Página escolar',
+      description: 'Demo de página',
+      imageUrl: itsur,
+      link: 'https://main--melodic-sundae-a09a6c.netlify.app/',
+    },
+    {
+      title: 'Acortador de URL',
+      description: 'Demo de consumo de APIs',
+      imageUrl: short,
+      link: 'https://cute-boba-d3d365.netlify.app/',
+    },
+    {
+      title: 'Ecommerce',
+      description: 'Demo de una tienda en linea',
+      imageUrl: ecomers,
+      link: 'https://lrwresearch.com/index.php',
+    },
+  ];
+
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 2,
+    slidesToScroll: 2,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  };
+
+  const carouselStyle = {
+    background: 'linear-gradient(190deg, #8B4780 0%, #3C0433 100%)',
+  };
+
+  return (
+    <div className="w-3/4 block mt-16 m-auto scroll-content fadeTop">
+      <h3 className='text-center text-2xl font-bold md:text-3xl block w-full'>Algunos Demos</h3>
+      <div style={carouselStyle}>
+        <Slider {...settings}>
+          {projects.map((project, index) => (
+            <div key={index} className="carousel-item">
+              <div className="max-w-sm m-auto">
+                <CardsProjects {...project} />
+              </div>
+            </div>
+          ))}
+        </Slider>
+      </div>
+    </div>
+  );
+};
