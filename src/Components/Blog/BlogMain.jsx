e55fb79043b3fd54991a2d5780bc6014be632242
[hugo gaytan] <[s19120145@alumnos.itsur.edu.mx]>
2024-01-12 16:09:05 -0600

    ravekh v 2.0

---
+++ src/Components/Blog/BlogMain.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const BlogMain = () => {
+  return <div>BlogMain</div>;
+};
--- src/Components/Muestra/Muestra.jsx
+++ src/Components/Muestra/Muestra.jsx
@@ -1,53 +1,53 @@
-import React from 'react';
-import { CardsProjects } from './CardsProjects';
-import mascotitas from '../../assets/MASCOTITAS.png';
-import filter from '../../assets/filter-cards.png';
-import itsur from '../../assets/ITSUR.png';
-import short from '../../assets/short-link.png';
-import webComida from '../../assets/webComida.png';
-import ecomers from '../../assets/ecomers.png';
-import Slider from 'react-slick';
-
-import 'slick-carousel/slick/slick.css';
-import 'slick-carousel/slick/slick-theme.css';
+import React, { useRef, useEffect } from "react";
+import { CardsProjects } from "./CardsProjects";
+import mascotitas from "../../assets/MASCOTITAS.png";
+import filter from "../../assets/filter-cards.png";
+import itsur from "../../assets/ITSUR.png";
+import short from "../../assets/short-link.png";
+import webComida from "../../assets/webComida.png";
+import ecomers from "../../assets/ecomers.png";
+import Slider from "react-slick";
+import "./Muestra.css";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 export const Muestra = () => {
   const projects = [
     {
-      title: 'Portal de comida',
-      description: 'Portal de comida (responsive)',
+      title: "Portal de comida",
+      description: "Portal de comida (responsive)",
       imageUrl: webComida,
-      link: 'https://main--stalwart-narwhal-93eefb.netlify.app/',
+      link: "https://main--stalwart-narwhal-93eefb.netlify.app/",
     },
     {
-      title: 'Mascotitas',
-      description: 'Demo de una tienda para mascotas',
+      title: "Mascotitas",
+      description: "Demo de una tienda para mascotas",
       imageUrl: mascotitas,
-      link: 'https://hugogaytan10.github.io/Mascotitas',
+      link: "https://hugogaytan10.github.io/Mascotitas",
     },
     {
-      title: 'Filtro de tarjetas',
-      description: 'Demo de un filtro de tarjetas',
+      title: "Filtro de tarjetas",
+      description: "Demo de un filtro de tarjetas",
       imageUrl: filter,
-      link: 'https://teal-jalebi-6ba797.netlify.app/',
+      link: "https://teal-jalebi-6ba797.netlify.app/",
     },
     {
-      title: 'Página universitaria',
-      description: 'Demo de página',
+      title: "Página universitaria",
+      description: "Demo de página",
       imageUrl: itsur,
-      link: 'https://main--melodic-sundae-a09a6c.netlify.app/',
+      link: "https://main--melodic-sundae-a09a6c.netlify.app/",
     },
     {
-      title: 'Acortador de URL',
-      description: 'Demo de consumo de APIs',
+      title: "Acortador de URL",
+      description: "Demo de consumo de APIs",
       imageUrl: short,
-      link: 'https://cute-boba-d3d365.netlify.app/',
+      link: "https://cute-boba-d3d365.netlify.app/",
     },
     {
-      title: 'Ecommerce',
-      description: 'Demo de una tienda en linea',
+      title: "Ecommerce",
+      description: "Demo de una tienda en linea",
       imageUrl: ecomers,
-      link: 'https://lrwresearch.com/index.php',
+      link: "https://lrwresearch.com/index.php",
     },
   ];
 
@@ -69,32 +69,83 @@ export const Muestra = () => {
   };
 
   const carouselStyle = {
-    cursor: 'grab',
+    cursor: "grab",
   };
 
   const handleMouseDown = () => {
-    const carru = document.getElementById('carru');
-    carru.style.cursor = 'grabbing';
+    const carru = document.getElementById("carru");
+    carru.style.cursor = "grabbing";
   };
 
   const handleMouseUp = () => {
-    const carru = document.getElementById('carru');
-    carru.style.cursor = 'grab';
+    const carru = document.getElementById("carru");
+    carru.style.cursor = "grab";
   };
+
+  const carruRef = useRef(null); // Referencia al div
+  const followerRef = useRef(null); // Referencia al seguidor del cursor
+  
+  useEffect(() => {
+    const div = carruRef.current;
+    const follower = followerRef.current;
+
+    const handleMouseMove = (e) => {
+      // Asegúrate de que el cálculo se basa en la posición relativa al div
+      const rect = div.getBoundingClientRect();
+      follower.style.left = e.clientX - rect.left + "px";
+      follower.style.top = e.clientY - rect.top + "px";
+
+
+    };
+
+    const handleMouseEnter = () => {
+      follower.style.display = "block";
+    };
+
+    const handleMouseLeave = () => {
+      follower.style.display = "none";
+    };
+
+    if (div) {
+      div.addEventListener("mousemove", handleMouseMove);
+      div.addEventListener("mouseenter", handleMouseEnter);
+      div.addEventListener("mouseleave", handleMouseLeave);
+
+      // Limpieza de eventos
+      return () => {
+        div.removeEventListener("mousemove", handleMouseMove);
+        div.removeEventListener("mouseenter", handleMouseEnter);
+        div.removeEventListener("mouseleave", handleMouseLeave);
+      };
+    }
+  }, []);
 
   return (
     <div className="w-3/4 block mt-16 m-auto scroll-content fadeTop">
-      <h3 className='text-center text-2xl font-bold md:text-3xl block w-full'>Algunos Demos</h3>
-      <div id="carru" style={carouselStyle} onMouseDown={handleMouseDown} onMouseUp={handleMouseUp}>
-        <Slider {...settings}>
+      <h3 className="text-center text-2xl font-bold md:text-3xl block w-full">
+        Algunos Demos
+      </h3>
+      <div
+        id="carru"
+        className="contenedor-carrusel overflow-hidden"
+        style={carouselStyle}
+        onMouseDown={handleMouseDown}
+        onMouseUp={handleMouseUp}
+        ref={carruRef}
+      >
+        <Slider {...settings} >
+          
           {projects.map((project, index) => (
-            <div key={index} className="carousel-item">
-              <div className="max-w-sm m-auto">
+            <div key={index} className="carousel-item" >
+              <div className="max-w-sm m-auto" >
                 <CardsProjects {...project} />
               </div>
             </div>
           ))}
         </Slider>
+      </div>
+      <div ref={followerRef} className="cursor-follower">
+       
       </div>
     </div>
   );
