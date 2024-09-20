23ce2ddbfe45717a96cc169e91cd46bb5aa94a69
[hugo gaytan] <[s19120145@alumnos.itsur.edu.mx]>
2024-09-19 18:12:30 -0600

    dos columnas

--- src/Components/CatalogoWeb/MainCatalogo.tsx
+++ src/Components/CatalogoWeb/MainCatalogo.tsx
@@ -63,7 +63,7 @@ useEffect(() => {
               </p>
             </div>
           ) : (
-            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+            <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-6">
               {Array.isArray(productos) &&
                 productos.map((producto, index) => (
                   <motion.div
