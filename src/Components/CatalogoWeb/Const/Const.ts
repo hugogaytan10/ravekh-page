9df3b24ce596b2b375b17863f8177bade865fa0d
Clementain <clentecarmona@gmail.com>
2026-02-09 22:44:04 -0600

    cambio nombre pantallas

--- src/Components/CatalogoWeb/Const/Const.ts
+++ src/Components/CatalogoWeb/Const/Const.ts
@@ -1,5 +1,5 @@
 export const URL =
 //'https://apipos.ravekh.com/api/'
 //    'https://api.ravekh.com/api/'
-'http://192.168.1.42:8090/api/'
+'http://10.0.0.10:8090/api/'
 
--- src/Components/CatalogoWeb/PuntoVenta/Const/Const.ts
+++ src/Components/CatalogoWeb/PuntoVenta/Const/Const.ts
@@ -1,6 +1,6 @@
 export const URL =
 //'https://apipos.ravekh.com/api/'
 //    'https://api.ravekh.com/api/'
-'http://192.168.1.42:8090/api/'
+'http://10.0.0.10:8090/api/'
 
  
