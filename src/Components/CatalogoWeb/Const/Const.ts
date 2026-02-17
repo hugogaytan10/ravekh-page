5e24fc80bb397bcfc15b3f0d6b6589339ab6f264
Clementain <clentecarmona@gmail.com>
2026-02-17 11:26:46 -0600

    urls

--- src/Components/CatalogoWeb/Const/Const.ts
+++ src/Components/CatalogoWeb/Const/Const.ts
@@ -1,6 +1,7 @@
 export const URL =
     //'https://apipos.ravekh.com/api/'
-    'https://api.nearvy.com.mx/api/'
+    // 'https://api.nearvy.com.mx/api/'
 // 'http://192.168.1.38:8090/api/'
+'http://127.0.1:8090/api/';
 
 
