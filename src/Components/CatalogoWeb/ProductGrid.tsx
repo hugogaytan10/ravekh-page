e8ff6b39d2af6567519135761308b5b6c08167d6
JoseMRM210801 <s19120164@alumnos.itsur.edu.mx>
2026-03-13 13:11:57 -0600

    se sube

--- src/Components/CatalogoWeb/ProductGrid.tsx
+++ src/Components/CatalogoWeb/ProductGrid.tsx
@@ -3,8 +3,8 @@ import { NavLink } from "react-router-dom";
 import { motion } from "framer-motion";
 import { Producto } from "./Modelo/Producto";
 import { getBaseVariantKey } from "./VariantSelectionModal";
-import cartIcon from "../../assets/POS/Car";
-import {Trash} from "../../assets/POS/trash";
+import cartIcon from "../../assets/cart-outline.svg";
+import {Trash} from "../../assets/POS/Trash.tsx";
 import { EyeIcon } from "../../assets/Cupones/icons/EyeIcon";
 interface ProductGridProps {
   products: Producto[];
