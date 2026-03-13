873becf619bda76d04a570e21fcb7f6d8181d105
JoseMRM210801 <s19120164@alumnos.itsur.edu.mx>
2026-03-13 14:00:36 -0600

    se cambiia la constante por la correcta

--- src/Components/CatalogoWeb/Cupones/shared/constants.ts
+++ src/Components/CatalogoWeb/Cupones/shared/constants.ts
@@ -1,2 +1,2 @@
 export const WEB_COUPONS_DOMAIN =
-  import.meta.env.VITE_WEB_COUPONS_DOMAIN ?? "https://69b46181e7d8340008088d68--tiny-smakager-a23d9c.netlify.app";
+  import.meta.env.VITE_WEB_COUPONS_DOMAIN ?? "https://tiny-smakager-a23d9c.netlify.app/";
