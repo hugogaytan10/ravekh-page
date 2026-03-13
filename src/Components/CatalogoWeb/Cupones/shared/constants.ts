2f0b11cb5569b5f057f4a65849e2450466fd8d54
JoseMRM210801 <s19120164@alumnos.itsur.edu.mx>
2026-03-13 13:15:34 -0600

    se agrega la constante a la web

--- src/Components/CatalogoWeb/Cupones/shared/constants.ts
+++ src/Components/CatalogoWeb/Cupones/shared/constants.ts
@@ -1,2 +1,2 @@
 export const WEB_COUPONS_DOMAIN =
-  import.meta.env.VITE_WEB_COUPONS_DOMAIN ?? "https://malachi-overeducative-tai.ngrok-free.dev";
+  import.meta.env.VITE_WEB_COUPONS_DOMAIN ?? "https://69b46181e7d8340008088d68--tiny-smakager-a23d9c.netlify.app";
