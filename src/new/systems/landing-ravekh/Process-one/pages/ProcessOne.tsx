262a4a692c756224f45fcfdf7f7e35c2af9ddc92
Clementain <s19120145@alumnos.itsur.edu.mx>
2026-03-18 13:25:09 -0600

    Inicio de nueva estructura de landing page

---
+++ src/new/systems/landing-ravekh/Process-one/pages/ProcessOne.tsx
@@ -0,0 +1,58 @@
+import React, { useRef, useEffect, useState } from "react";
+import "../../shared/styles/circleAnimated.css";
+import lupa from "../../assets/lupa.png";
+import { AnimatedText } from "../../utilities/AnimatedText"
+export const BannerSecundario = () => {
+  const secundario = useRef(null); // Referencia al div
+  const [shouldAnimate, setShouldAnimate] = useState(true);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      ([entry]) => {
+        if (entry.isIntersecting) {
+          setShouldAnimate(false); // Reset animation
+          setTimeout(() => {
+            setShouldAnimate(true); // Trigger animation
+          }, 50); // Delay slightly to allow DOM update
+        } 
+      },
+      { threshold: 1 } // Ajusta el threshold según necesites
+    );
+
+    if (secundario.current) {
+      observer.observe(secundario.current);
+    }
+    return () => {
+      if (secundario.current) {
+        observer.unobserve(secundario.current);
+      }
+    };
+  }, []);
+  return (
+
+      <div
+        id="secundario"
+        ref={secundario}
+        className="w-full relative flex flex-wrap  justify-between min-h-screen md:items-center"
+      >
+        <span className=" block w-full text-white text-sm text-center absolute top-4 rombo">
+          Proceso 1
+        </span>
+        <h2 className=" text-gray-50 font-bold text-4xl  w-full md:w-1/4 text-center mb-10">
+          {shouldAnimate && <AnimatedText text="Análisis" />}
+        </h2>
+        <div className="relative w-full md:w-1/4 flex items-center justify-center">
+          <img
+            src={lupa}
+            alt=""
+            className="img-circle relative z-10 h-52 md:h-96 md:w-96 object-contain"
+          />
+          <div className="circle-of-dots"></div>
+        </div>
+        <p className="font-thin text-gray-50 mt-5 w-full md:w-1/4  p-1 ml-8 text-left text-base md:text-xl">
+          Comenzamos con la determinación de comprender su marca y los
+          intrincados detalles de cada proyecto único.
+        </p>
+      </div>
+  );
+};
