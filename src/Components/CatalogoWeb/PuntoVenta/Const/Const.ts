56aa6741968512213b4508e7fd5e2735aaa533c6
JoseMRM210801 <s19120164@alumnos.itsur.edu.mx>
2026-03-25 14:34:07 -0600

    correccion de rutas

--- src/Components/CatalogoWeb/PuntoVenta/Const/Const.ts
+++ src/Components/CatalogoWeb/PuntoVenta/Const/Const.ts
@@ -1,7 +1,7 @@
 export const URL =
     //'https://apipos.ravekh.com/api/'
-    //'https://apipos.ravekh.com/api/'
-   'http://192.168.1.82:8090/api/'
+    'https://apipos.ravekh.com/api/'
+   //'http://192.168.1.82:8090/api/'
     //'http://127.0.1:8090/api/';
     
 
